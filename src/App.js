a2606d60f89284be40aff12d783133d7f60a0458
@douglasabnovato <douglasabnovato@gmail.com>
2019-02-26 16:52:55 -0300

    criando projeto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+
+import Site from './Site';
+import Rodape from './components/Rodape';
+import Cabecalho from './components/Cabecalho';
+import Inicio from './components/Inicio';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <Cabecalho />
+          <Inicio />
+          <Site />
+          <Rodape />
       </div>
     );
   }
