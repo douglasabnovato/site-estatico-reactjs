69311ddcb50c1d2db85a80f0684a15c5b53e7b50
@douglasabnovato <douglasabnovato@gmail.com>
2019-02-27 18:32:57 -0300

    aulas até a 10

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React, { Component } from 'react';
-
+import {BrowserRouter, Route} from 'react-router-dom';
 
 import Site from './Site';
 import Rodape from './components/Rodape';
 import Cabecalho from './components/Cabecalho';
 import Inicio from './components/Inicio';
+import Servicos from './components/Servicos';
+import Portfolio from './components/Portfolio';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-          <Cabecalho />
-          <Inicio />
-          <Site />
-          <Rodape />
-      </div>
+      <BrowserRouter >
+        <div className="App">
+            <Cabecalho />
+
+            <Route path='/' exact component={Inicio} />
+            <Route path='/servicos' component={Servicos}/>
+            <Route path='/portfolio' component={Portfolio}/>
+            
+            <Rodape />
+        </div>
+      </BrowserRouter>
     );
   }
 }
--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -2,21 +2,8 @@ import React from 'react'
 
 const Inicio = props => {
     return (
-        <div>
-            <div className='jumbotron text-center'>
-                <h1>Company</h1>
-                <p>We specialize in blablabla</p>
-                <form>
-                    <div className='input-group'>
-                        <input type='email' className='form-control' size='50' placeholder='Email Address' required />
-                        <div className='input-group-btn'>
-                            <button type='button' className='btn btn-danger'>Subscribe</button>
-                        </div>
-                    </div>
-                </form>
-            </div>
-
-            {/* Container (About Section) */}
+        <div>            
+             
             <div id='about' className='container-fluid'>
                 <div className='row'>
                     <div className='col-sm-8'>
@@ -27,6 +14,20 @@ const Inicio = props => {
                     </div>
                     <div className='col-sm-4'>
                         <span className='glyphicon glyphicon-signal logo'></span>
+                    </div>
+                </div>
+            </div>
+
+            <div className='container-fluid bg-grey'>
+                <div className='row'>
+                    <div className='col-sm-4'>
+                        <span className='glyphicon glyphicon-globe logo '></span>
+                    </div>
+                    <div className='col-sm-8'>
+                        <h2>Our Values</h2><br />
+                        <h4><strong>MISSION:</strong> Our mission lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</h4><br />
+                        <p><strong>VISION:</strong> Our vision Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                     </div>
                 </div>
             </div>
